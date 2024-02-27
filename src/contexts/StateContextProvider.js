7a8e3364581b51c44be9b32cb40934f6fe1b05f6
Ayoun-Elaouadi <ayoubelaouadi16@gmail.com>
2024-02-27 21:00:04 +0100

    fc

--- src/contexts/StateContextProvider.js
+++ src/contexts/StateContextProvider.js
@@ -1,28 +1,38 @@
 import React, { createContext, useContext, useState } from 'react';
+import axios from 'axios';
 
 const StateContext = createContext();
-const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+const baseUrl = 'https://google-search74.p.rapidapi.com';
 
 export const StateContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
 
-  const getResults = async (url) => {
+  const getResults = async () => {
     setLoading(true);
 
-    const res = await fetch(`${baseUrl}${url}`, {
+    const options = {
       method: 'GET',
-      headers: {
-        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
+      url: baseUrl,
+      params: {
+        query: searchTerm,
+        limit: '10',
+        related_keywords: 'true'
       },
-    });
+      headers: {
+        'X-RapidAPI-Key': 'f874d0a201mshfb0ce78a0ed7d66p145d33jsn6f5e4b21cd0b',
+        'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+      }
+    };
 
-    const data = await res.json();
-
-    setResults(data);
-    setLoading(false);
+    try {
+      const response = await axios.request(options);
+      setResults(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import { StateContextProvider } from './contexts/StateContextProvider';
 import './global.css';
 
+
 ReactDom.render(
   <StateContextProvider>
     <Router>
